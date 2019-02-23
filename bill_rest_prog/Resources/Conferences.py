caaefa74df390581f8f3d0f2278a27fb71f1e725
Steve Cobb <scobb7@nc.rr.com>
2019-02-23 16:18:07 -0500

    The framework is setup

--- bill_rest_prog/Resources/Conferences.py
+++ bill_rest_prog/Resources/Conferences.py
@@ -1,31 +1,28 @@
 from flask_restful import Resource
 from flask_restful import abort
 
+import DataBase
+
 class Conference( Resource ):
-    def __init__(self):
-       self.msg = {'name':'myname'}
-       self.msg2 = {'name': 'othername'}
+    def get(self, callid):
+       ''' returns information on single record '''
 
-
-    def get(self, name):
-      return {'nameis': name}
+       # setup sqlalchemy here
+       rsp = DataBase.get_conference_by_callId(callid)
+       return rsp
 
 
 class Conferences( Resource ):
-    def __init__(self):
-       self.msg = {'name':'myname'}
-       self.msg2 = {'name': 'othername'}
+  def get(self, iptype ):
+      ''' returns information on a local or remote ip address '''
 
-
-    def get(self):
-      return {'nameis': 'allconference'}
+      #   setup the select, sqlalchemy
+      rsp = DataBase.get_conference_local_remote(iptype)
+      return rsp
 
 
 class AllConferences( Resource ):
-    def __init__(self):
-       self.msg = {'name':'myname'}
-       self.msg2 = {'name': 'othername'}
-
-
     def get(self):
-      return {'nameis': 'allconference'}
+        rsp = DataBase.get_all_conferences()
+        return rsp
+
